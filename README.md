6ddd903975d74f394675d875fd760536a76fb481
ImsirovicAjdin <ajdika@live.com>
2024-04-25 17:10:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-js-collapsible-sidebar
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ImsirovicAjdin/react-js-collapsible-sidebar)
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './style.css';
+
+function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="app">
+      <div className={`sidebar ${isSidebarOpen ? '' : 'collapsed'}`}>
+        <button onClick={toggleSidebar} className="toggle-btn">Toggle</button>
+      </div>
+      <div className="main">
+        <h1>Table Heading</h1>
+        <table>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+          </tr>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+          </tr>
+          <tr>
+            <td>Data 4</td>
+            <td>Data 5</td>
+            <td>Data 6</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
